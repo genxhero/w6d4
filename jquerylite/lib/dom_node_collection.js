a32178300a03f7e25d638d56135042c4e5996432
aarongoddard <aaronbear@gmail.com>
2018-09-27 14:22:26 -0700

    initial commit

---
+++ jquerylite/lib/dom_node_collection.js
@@ -0,0 +1,17 @@
+class DOMNodeCollection {
+  constructor(elements) {
+    this.elArr = elements;
+  }
+
+  html(){
+  }
+
+  empty() {
+  }
+
+  append() {
+    
+  }
+}
+
+module.exports = DOMNodeCollection;
---
+++ jquerylite/lib/main.js
@@ -0,0 +1,5 @@
+DOMNodeCollection = require('./dom_node_collection.js');
+
+ window.$l = (selector) => {
+  console.log("this is only a test");
+};
